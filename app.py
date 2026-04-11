5aa0d7ca079c304ab4cc823123262e741c4bfde6
Ashish Mehara <viratmehara232@gmail.com>
2026-04-11 10:18:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ st.markdown("""
 /* Background Image Updated */
 .stApp {
     background: linear-gradient(rgba(0,0,0,0.75), rgba(0,0,0,0.9)),
-    url("https://images.unsplash.com/photo-1743767587687-9ebaac2b55e3?q=80&w=1355&auto=format&fit=crop");
+    url("https://images.unsplash.com/photo-1523243319451-54b60322f948?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
     background-size: cover;
     background-position: center;
 }
